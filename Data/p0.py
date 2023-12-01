27329060ee67bf29678f115763385aa1c7998ef0
Jayshil <jayshilpatel147@gmail.com>
2023-12-01 22:05:59 +0100

    variability analysis

--- Data/p0.py
+++ Data/p0.py
@@ -7,14 +7,14 @@ import os
 tim, fl, fle = juliet.utils.get_all_TESS_data('WASP-18')
 
 # For all data
-for i in ['TESS2', 'TESS3']:
+for i in tim.keys():
     f1 = open(os.getcwd() + '/Data/WASP-18_' + str(i) + '.dat', 'w')
     for j in range(len(tim[i])):
         f1.write(str(tim[i][j]) + '\t' + str(fl[i][j]) + '\t' + str(fle[i][j]) + '\n')
     f1.close()
 
 # For only eclipse data
-for i in ['TESS2', 'TESS3']:
+for i in tim.keys():
     per, t0, t14 = 0.94145223, 2456740.80560, 2.21/24
     phs = juliet.utils.get_phases(tim[i], per, t0)
     mask = np.where(np.abs(phs*per) >= t14)[0]
